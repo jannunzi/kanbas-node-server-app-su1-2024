16ee415ac32b85273239681c281a061daae9ada9
Jose Annunziato <jannunzi@gmail.com>
2024-06-13 19:29:22 -0400

    a6

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
+import "dotenv/config";
 import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import cors from "cors";
+import UserRoutes from "./User/routes.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas-su1";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors());
@@ -13,5 +20,6 @@ Hello(app);
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);
